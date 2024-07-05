9146a6403aad04384c1c4d9b943f6500fa2d5700
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-07-04 20:57:05 -0600

    elaboracion de vistas

--- resources/views/livewire/Nacionalidad/create.blade.php
+++ resources/views/livewire/Nacionalidad/create.blade.php
@@ -11,13 +11,22 @@
             role="dialog" aria-modal="true" aria-labelledby="modal-headline">
             <form>
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                Nacionalidad
+                </h3>
+                <button  wire:click="closeModal()" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
                     <div class="">
                         <div class="mb-4">
                             <label for="exampleFormControlInput1"
                                 class="block text-gray-700 text-sm font-bold mb-2">Nacionalidad:</label>
                             <input type="text"
                                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="exampleFormControlInput1" placeholder="Nacionalidad" wire:model="nombreNacionalidad">
+                                id="exampleFormControlInput1" placeholder="Ingresar nacionalidad" wire:model="nombreNacionalidad">
                             @error('nombreNacionalidad') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
