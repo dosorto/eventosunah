2d9fa9f58988460c4daa5a49a2ce0c6734682c8a
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-07-04 18:04:03 -0600

    elaboracion de vistas

---
+++ app/Livewire/Nacionalidad/Nacionalidades.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Livewire\Nacionalidades;
+use Livewire\WithPagination;
+use Livewire\Component;
+use App\Models\Nacionalidad;
+
+class Nacionalidades extends Component
+{
+    use WithPagination;
+    public $nombreNacionalidad, $nacionalidad_id, $search;
+    public $isOpen = 0;
+    public function render()
+    {
+        $nacionalidades = Nacionalidad::where('nombreNacionalidad', 'like', '%'.$this->search.'%')->orderBy('id','DESC')->paginate(5);
+        return view('livewire.Nacionalidad.nacionalidades', ['nacionalidades' => $nacionalidades]);
+    }
+    public function create()
+    {
+        $this->resetInputFields();
+        $this->openModal();
+    }
+    public function openModal()
+    {
+        $this->isOpen = true;
+    }
+    public function closeModal()
+    {
+        $this->isOpen = false;
+    }
+    private function resetInputFields(){
+        $this->nombreNacionalidad = '';
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'nombreNacionalidad' => 'required',
+        ]);
+   
+        Nacionalidad::updateOrCreate(['id' => $this->nacionalidad_id], [
+            'nombreNacionalidad' => $this->nombreNacionalidad,
+        ]);
+  
+        session()->flash('message', 
+            $this->nacionalidad_id ? 'Nacionalidad Actualizada correctamente!' : 'Nacionalidad creada correctamente!');
+  
+        $this->closeModal();
+        $this->resetInputFields();
+    }
+    public function edit($id)
+    {
+        $nacionalidad = Nacionalidad::findOrFail($id);
+        $this->nacionalidad_id = $id;
+        $this->nombreNacionalidad = $nacionalidad->nombreNacionalidad;
+    
+        $this->openModal();
+    }
+     
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function delete($id)
+    {
+        Nacionalidad::find($id)->delete();
+        session()->flash('message', 'Registro Eliminado correctamente!');
+    }
+}
--- app/Models/Nacionalidad.php
+++ app/Models/Nacionalidad.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Nacionalidad extends Model
+class Nacionalidad extends BaseModel
 {
     use HasFactory;
     use SoftDeletes;
