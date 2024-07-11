8a54ac21ba02bed4bb456b057f7b053a151c8169
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-07-10 19:35:12 -0600

    Modificaciones internas de las plantillas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -423,19 +423,6 @@
             container.classList.remove("active");
         });
     </script>
-     <script>
-    const header = document.querySelector("header");
-    const hamburgerBtn = document.querySelector("#hamburger-btn");
-    const closeMenuBtn = document.querySelector("#close-menu-btn");
-
-    // Toggle mobile menu on hamburger button click
-    hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
-
-    // Close mobile menu on close button click
-    closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
-  </script>
-
-</body>
 </body>
 
 </html>
