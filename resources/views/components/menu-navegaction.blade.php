21d62f7d0074685fdc8dbc50880573a74d836035
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-07-14 19:21:50 -0600

    Logo

--- resources/views/components/menu-navegaction.blade.php
+++ resources/views/components/menu-navegaction.blade.php
@@ -16,7 +16,7 @@
                </button>
                <a href="/dashboard" class="flex ms-2 md:me-24">
                   <img src="{{ asset('Logo/Eventos_UNAH_Logo.png') }}" alt="Logo" height="70px" width="70px" />
-                  <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white"></span>
+                  <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">EVENTOS UNAH</span>
                </a>
 
             </div>
