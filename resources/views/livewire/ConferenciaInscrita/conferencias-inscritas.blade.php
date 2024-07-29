4df82a59fa1350884a39adbd63a5aee6e5f44375
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-07-28 21:06:25 -0600

    Asistencia modificaciones en las relaciones

--- resources/views/livewire/ConferenciaInscrita/conferencias-inscritas.blade.php
+++ resources/views/livewire/ConferenciaInscrita/conferencias-inscritas.blade.php
@@ -285,9 +285,8 @@
                                 <svg class="w-6 h-6 text-gray-800 dark:text-gray-800" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M21 9H8a5 5 0 0 0 0 10h9m4-10-4-4m4 4-4 4" />
+                                        d="M3 9h13a5 5 0 0 1 0 10H7M3 9l4-4M3 9l4 4" />
                                 </svg>
-
                             </button>
                             <button wire:click="asistencia({{ $suscripcion->id }})"
                                 class="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-gray-800 bg-green-500 rounded-lg hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700">
