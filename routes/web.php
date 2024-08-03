f054de2cf5fc2686a4c5051312d1d75e2cd14bac
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-08-03 16:05:58 -0600

    Reapply "Merge Adair"
    This reverts commit ecc0dfb370188eccaa81f0d7453bb239614a3ec6.

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,10 @@ Route::middleware([
     //  Route::get('/usuario', Usuarios::class)->name('usuario');
     Route::get('/evento/{evento}/conferencias', VistaConferencias::class)->name('vistaconferencia');
     Route::get('/conferencias-inscritas', ConferenciasInscritas::class)->name('conferencias-inscritas');
-    Route::get('/evento/{evento}/reporteEvento', ReporteEventos::class)->name('reporteEvento');
+    Route::get('/vistaDiploma/asistencia/{asistencia?}', VistaDiplomas::class)->name('vistaDiploma');
+    
+    // VALIDAR EL DIPLOMA
+    Route::get('/validarDiploma/{asistencia}', [EventoVistaController::class, 'validarDiploma'])->name('validarDiploma');
 });
 
 Route::get('/registrar', [RegistrarUsarioController::class, 'index'])->name('register');
