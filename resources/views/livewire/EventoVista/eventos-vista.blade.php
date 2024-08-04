04d33d13ad557bee52dfdd0929fb66fb623f09c6
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-08-03 19:18:27 -0600

    Asitencia y boton enviar diploma

--- resources/views/livewire/EventoVista/eventos-vista.blade.php
+++ resources/views/livewire/EventoVista/eventos-vista.blade.php
@@ -39,12 +39,12 @@
       @foreach($targetasEventos as $targetasEvento)
       <a href="{{ route('vistaconferencia', ['evento' => $targetasEvento->id]) }}" class="evento-card">
       <div class="thumbnail-container">
-      @if($targetasEvento->logo = null)
-      <img src="{{ asset(str_replace('public', 'storage', $targetasEvento->logo)) }}" alt="Sin Foto De Evento"
-      class=" bg-white dark:bg-gray-800  dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white thumbnail">
-    @else
+      @if($targetasEvento->logo == "")
       <img src="http://www.puertopixel.com/wp-content/uploads/2011/03/Fondos-web-Texturas-web-abtacto-17.jpg"
       alt="Sin Foto De Evento">
+    @else
+      <img src="{{ asset(str_replace('public', 'storage', $targetasEvento->logo)) }}" alt="Sin Foto De Evento"
+      class=" bg-white dark:bg-gray-800  dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white thumbnail">
     @endif
       <p class="marca">{{ $targetasEvento->modalidad->modalidad }}</p>
       </div>
