24e45576e3ce4d5b6ca83611de718b205b655b61
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-08-13 23:20:18 -0600

    Plnatillas diplomas

--- resources/views/livewire/validar-diploma.blade.php
+++ resources/views/livewire/validar-diploma.blade.php
@@ -4,20 +4,23 @@
             class="p-4 mb-4 text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800"
             role="alert">
             <div class="flex items-center">
-                <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor" viewBox="0 0 20 20">
-                    <path
-                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                <svg class="w-12 h-12 text-green-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd"
+                        d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z"
+                        clip-rule="evenodd" />
                 </svg>
+
                 <span class="sr-only">Info</span>
-                <h3 class="text-lg font-medium">Validación de Diploma</h3>
+                <h3 class="text-lg font-medium">DIPLOMA VERIFICADO</h3>
             </div>
             <div class="mt-2 mb-4 text-sm">
-                <p><strong>Nombre:</strong> {{ $persona->nombre }} {{$persona->apellido}}</p>
+                <p><strong>Otorgado a:</strong> {{ $persona->nombre }} {{$persona->apellido}}</p>
                 <p><strong>Conferencia:</strong> {{ $conferencia->nombre }}</p>
                 <p><strong>Código Diploma:</strong> {{ $codigoDiploma->Codigo }}</p>
                 <strong>Este diploma es válido y está asociado a la conferencia.</strong>
             </div>
+
             <div class="flex">
                 <a href="{{ route('vistaDiploma', ['asistencia' => $asistencia->id]) }}"
                     class="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
@@ -35,20 +38,21 @@
             class="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
             role="alert">
             <div class="flex items-center">
-                <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor" viewBox="0 0 20 20">
-                    <path
-                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                <svg class="w-12 h-12 text-red-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                        d="m6 6 12 12m3-6a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
+
                 <span class="sr-only">Info</span>
-                <h3 class="text-lg font-medium">Diploma no válido</h3>
+                <h3 class="text-lg font-medium">DIPLOMA NO VÁLIDO</h3>
             </div>
             <div class="mt-2 mb-4 text-sm">
-                Este diploma no está verificado, cualquier copia impresa de este documento no tiene  
+                Este diploma no está verificado, cualquier copia impresa de este documento no tiene
                 validez legal ni jurídica sin el respaldo de la página electrónica.
             </div>
             <div class="flex">
-            <a href="{{ route('eventoVista') }}"
+                <a href="{{ route('eventoVista') }}"
                     class="text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                     <svg class="me-2 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                         viewBox="0 0 20 14">
@@ -57,7 +61,7 @@
                     </svg>
                     Regresar
                 </a>
-                
+
             </div>
         </div>
     @endif
