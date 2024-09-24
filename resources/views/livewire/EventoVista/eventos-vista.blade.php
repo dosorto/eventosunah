92d5e464e2e47a347736df674b3e827ea9cd1990
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-09-23 23:19:13 -0600

    Boton suscribir en eventos pagados

--- resources/views/livewire/EventoVista/eventos-vista.blade.php
+++ resources/views/livewire/EventoVista/eventos-vista.blade.php
@@ -42,12 +42,29 @@
       <img src="{{ asset('Logo/EVENTIS LOGO.png') }}" alt="foto-creador" class="icon">
       <div class="evento-details">
       <h2 class="name-evento">{{ $tarjetasEvento->nombreevento }}</h2>
-      <p class="evento-creador">{{ $tarjetasEvento->organizador }}</p>
-      <p class="inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">Inscribirse</p>
-      <p class="fecha-creacion">{{ $tarjetasEvento->created_at->diffForHumans() }}</p>
-     {{-- @if ($tarjetasEvento->estado === 'Pagado')
-            
-      @endif --}}
+      <div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6">
+        <div class="flex flex-col justify-center max-w-sm">
+        <p class="evento-creador">{{ $tarjetasEvento->organizador }}</p>
+        <p class="fecha-creacion">{{ $tarjetasEvento->created_at->diffForHumans() }}</p>
+        </div>
+
+        <div class="flex flex-col justify-center max-w-sm">
+        @if ($tarjetasEvento->estado === 'Pagado')
+      <p
+      class="inline-flex md:ml-0 md:mr-8 sm:ml-0 sm:mr-8 lg:ml-8 lg:mr-0 items-center px-3 py-2 text-sm font-semibold text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+      Inscribirse
+      <svg class="w-6 h-6 text-gray-800 dark:text-gray-800" aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+      <path fill-rule="evenodd"
+        d="M5 8a4 4 0 1 1 7.796 1.263l-2.533 2.534A4 4 0 0 1 5 8Zm4.06 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h2.172a2.999 2.999 0 0 1-.114-1.588l.674-3.372a3 3 0 0 1 .82-1.533L9.06 13Zm9.032-5a2.907 2.907 0 0 0-2.056.852L9.967 14.92a1 1 0 0 0-.273.51l-.675 3.373a1 1 0 0 0 1.177 1.177l3.372-.675a1 1 0 0 0 .511-.273l6.07-6.07a2.91 2.91 0 0 0-.944-4.742A2.907 2.907 0 0 0 18.092 8Z"
+        clip-rule="evenodd" />
+      </svg>
+      </p>
+    @endif
+        </div>
+      </div>
+
+
       </div>
       </div>
       </a>
