70d545d3de778664bf78f60bb7be98b4297e42c4
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-09-23 21:08:22 -0600

    mejoras

--- resources/views/pagina-inicial.blade.php
+++ resources/views/pagina-inicial.blade.php
@@ -2,33 +2,33 @@
 @section('app-content')
 <section class="dark:bg-gray-900 mx-auto sm:px-6 lg:px-8">
     <x-nav />
-    <div id="default-carousel" class="relative z-10 w-full mt-20" data-carousel="static">
+    <div id="default-carousel" class="relative z-0 w-full mt-20" data-carousel="static">
         <!-- Carousel wrapper -->
         <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
             <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="{{ asset('Logo/baner1.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
             <!-- Item 2 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="{{ asset('Logo/logoUNAH.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
             <!-- Item 3 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="{{ asset('Logo/baner1.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
             <!-- Item 4 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('Logo/flyer2.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <img src="{{ asset('Logo/camarones.jpg') }}"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
             <!-- Item 5 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="{{ asset('Logo/baner1.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
         </div>
         <!-- Slider indicators -->
@@ -76,14 +76,14 @@
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 m-8">
 
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img class="w-full h-60 object-cover rounded-t-lg" src="{{ asset('Logo/conferencistas.jpg') }}"
-                    alt="" />
+            <img class="w-full h-60 object-cover rounded-t-lg" src="{{ asset('Logo/conferencistas.jpg') }}" alt="" />
             <div class="p-5">
                 <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Conoce más
                         sobre nuestros conferencistas</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Cada una de las conferencias será impartida por expertos
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Cada una de las conferencias será impartida
+                    por expertos
                     e investigadores nacionales e internacionales.</p>
                 <a href="#"
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -97,7 +97,7 @@
             </div>
         </div>
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img class="w-full h-60 object-cover rounded-t-lg" src="{{ asset('Logo/camarones.jpg') }}" alt="" />
+            <img class="w-full h-60 object-cover rounded-t-lg" src="{{ asset('Logo/camarones.jpg') }}" alt="" />
             <div class="p-5">
                 <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Descubre
@@ -117,13 +117,14 @@
             </div>
         </div>
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img class="w-full h-60 object-cover rounded-t-lg" src="{{ asset('Logo/choluteca2.jpg') }}" alt="" />
+            <img class="w-full h-60 object-cover rounded-t-lg" src="{{ asset('Logo/choluteca2.jpg') }}" alt="" />
             <div class="p-5">
                 <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Métodos de
                         inscripción y pagos</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Aquí puedes ver el formulario de inscripción y los métodos disponibles para realizar tu pago y subir comprobante.</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Aquí puedes ver el formulario de
+                    inscripción y los métodos disponibles para realizar tu pago y subir comprobante.</p>
 
                 <a href="#"
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -137,7 +138,7 @@
             </div>
         </div>
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img class="w-full h-60 object-cover rounded-t-lg" src="{{ asset('Logo/expoferia.jpg') }}" alt="" />
+            <img class="w-full h-60 object-cover rounded-t-lg" src="{{ asset('Logo/expoferia.jpg') }}" alt="" />
             <div class="p-5">
                 <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Expoferia
@@ -159,41 +160,37 @@
 
     </div>
 
-    <div id="default-carousel" class="relative w-full mb-8" data-carousel="static">
+    <div id="default-carousel" class="relative z-0 w-full" data-carousel="slide">
         <!-- Carousel wrapper -->
         <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
             <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('Logo/laguna.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <img src="{{ asset('Logo/choluteca2.jpg') }}"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
             <!-- Item 2 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('Logo/choluteca2.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <img src="{{ asset('Logo/hotel.jpg') }}"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
             <!-- Item 3 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('Logo/camarones.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <img src="{{ asset('Logo/laguna.png') }}"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
             <!-- Item 4 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('Logo/cholu.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <img src="{{ asset('Logo/expoferia.jpg') }}"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
             <!-- Item 5 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('Logo/hotel.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('Logo/lagunaartifi.jpeg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <img src="{{ asset('Logo/camarones.jpg') }}"
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-0" alt="...">
             </div>
         </div>
         <!-- Slider indicators -->
-        <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        <div class="absolute z-40 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
             <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
                 data-carousel-slide-to="0"></button>
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
