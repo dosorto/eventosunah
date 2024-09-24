e300197002908bb558751d0f4c9709ffa0514933
MAR2412 <diazfer375@gmail.com>
2024-09-23 19:19:19 -0600

    Inicio

---
+++ app/Models/Inscripcion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Suscripcion extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    
+    protected $fillable = ['IdEvento', 'IdPersona', 'created_by', 'deleted_by'];
+
+    public function evento()
+    {
+        return $this->belongsTo(Conferencia::class, 'IdConferencia');
+    }
+
+    public function persona()
+    {
+        return $this->belongsTo(Persona::class, 'IdPersona');
+    }
+
+
+
+}
