a1244e8ba52ef6b6da66fd5d46dccbaa324622f9
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-09-24 23:39:29 -0600

    modales y redireccionamiento

--- resources/views/livewire/EventoVista/eventos-vista.blade.php
+++ resources/views/livewire/EventoVista/eventos-vista.blade.php
@@ -45,8 +45,9 @@
                                         </div>
                                         <div class="flex flex-col justify-center max-w-sm">
                                             @if ($tarjetasEvento->estado === 'Pagado')
-                                                <p data-modal-target="progress-modal" data-modal-toggle="progress-modal"
-                                                    class="inscribir-button inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+                                                <p data-modal-target="progress-modal-{{ $tarjetasEvento->id }}"
+                                                    data-modal-toggle="progress-modal-{{ $tarjetasEvento->id }}"
+                                                    class="cursor-pointer inscribir-button inline-flex items-center px-3 py-2 text-sm font-semibold text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
                                                     Inscribirse
                                                     <svg class="w-6 h-6 text-gray-800 dark:text-gray-800" xmlns="http://www.w3.org/2000/svg"
                                                         fill="currentColor" viewBox="0 0 24 24">
@@ -62,7 +63,33 @@
                             </div>
 
                         </div>
-
+                        <div id="progress-modal-{{ $tarjetasEvento->id}}" tabindex="-1" aria-hidden="true"
+                            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div class="relative p-4 w-full max-w-md max-h-full">
+                                <!-- Modal content -->
+                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <div class="p-4 md:p-5">
+                                        <h3 class="mb-1 text-xl font-bold text-gray-900 dark:text-white">
+                                            {{$tarjetasEvento->nombreevento}} debe
+                                            ser pagado
+                                        </h3>
+                                        <p class="text-gray-500 dark:text-gray-400 mb-6">Por favor, sube el recibo de pago para
+                                            completar tu
+                                            inscripción.
+                                        <p>
+                                            <!-- Modal footer -->
+                                        <div class="flex items-center mt-6 space-x-4 rtl:space-x-reverse">
+                                            <button data-modal-hide="progress-modal-{{ $tarjetasEvento->id }}" type="button"
+                                                class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancelar</button>
+                                            <a href="{{ route('recibo', ['evento' => $tarjetasEvento->id]) }}" data-modal-hide="progress-modal-{{ $tarjetasEvento->id }}"
+                                                type="button"
+                                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Subir
+                                                Comprobante</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     @endforeach
             </div>
 
@@ -70,39 +97,4 @@
             {{ $Eventos->links() }}
             <br>
         @endif
-</div>
-
-<div id="progress-modal" tabindex="-1" aria-hidden="true"
-    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button"
-                class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-hide="progress-modal">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <div class="p-4 md:p-5">
-                <h3 class="mb-1 text-xl font-bold text-gray-900 dark:text-white">El evento debe ser pagado</h3>
-                <p class="text-gray-500 dark:text-gray-400 mb-6">Por favor, sube el recibo de pago para completar tu
-                    inscripción.
-                <p>
-                    <!-- Modal footer -->
-                <div class="flex items-center mt-6 space-x-4 rtl:space-x-reverse">
-                    <a href="{{route('recibo', $tarjetasEvento->id)}}" data-modal-hide="progress-modal" type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Subir
-                        Comprobante</a>
-                    <button data-modal-hide="progress-modal" type="button"
-                        class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancelar</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 </div>
