139d25cc42a33ecbfd96382893b074c2667e87ae
Acxel Aplicano <acxel.aplicano@unah.hn>
2024-09-25 22:12:41 -0600

    modal

--- resources/views/livewire/VistaConferencia/vista-conferencia.blade.php
+++ resources/views/livewire/VistaConferencia/vista-conferencia.blade.php
@@ -207,7 +207,8 @@
                                     <div id="course-modal" tabindex="-1" aria-hidden="true"
                                         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                         <div class="relative p-4 w-full max-w-lg max-h-full">
-                                            <!-- Modal content -->
+                                        <div class="fixed inset-0 bg-black opacity-50"></div>   
+                                        <!-- Modal content -->
                                             <div class="relative bg-white rounded-lg shadow dark:bg-gray-800">
                                                 <!-- Modal header -->
                                                 <div class="flex items-center justify-between p-4 md:p-5">
